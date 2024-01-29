d3ec5085c62429c6198c8116b839c0df2fce645e
Alred <prottoyrudra7777@gmail.com>
2024-01-29 04:49:44 +0000

    added download button

--- src/app/b-link-details/page.tsx
+++ src/app/b-link-details/page.tsx
@@ -2,11 +2,53 @@
 "use client"
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { Table, TableContainer, TableHead, TableBody, TableRow, TableCell, Paper } from '@mui/material';
+import { Table, TableContainer,Button, TableHead, TableBody, TableRow, TableCell, Paper } from '@mui/material';
 import { getFirestore,limit, collection, query, where, orderBy, getDocs } from 'firebase/firestore';
 import firebase_app from "../../firebase/config";
 import CircularProgress from "@mui/material/CircularProgress";
 import { useSearchParams } from 'next/navigation'
+
+// @ts-expect-error
+function exportToCsv(filename, rows) {
+  var processRow = function (row) {
+      var finalVal = '';
+      for (var j = 0; j < row.length; j++) {
+          var innerValue = row[j] === null ? '' : row[j].toString();
+          if (row[j] instanceof Date) {
+              innerValue = row[j].toLocaleString();
+          };
+          var result = innerValue.replace(/"/g, '""');
+          if (result.search(/("|,|\n)/g) >= 0)
+              result = '"' + result + '"';
+          if (j > 0)
+              finalVal += ',';
+          finalVal += result;
+      }
+      return finalVal + '\n';
+  };
+
+  var csvFile = '';
+  for (var i = 0; i < rows.length; i++) {
+      csvFile += processRow(rows[i]);
+  }
+
+  var blob = new Blob([csvFile], { type: 'text/csv;charset=utf-8;' });
+  if (navigator.msSaveBlob) { // IE 10+
+      navigator.msSaveBlob(blob, filename);
+  } else {
+      var link = document.createElement("a");
+      if (link.download !== undefined) { // feature detection
+          // Browsers that support HTML5 download attribute
+          var url = URL.createObjectURL(blob);
+          link.setAttribute("href", url);
+          link.setAttribute("download", filename);
+          link.style.visibility = 'hidden';
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+      }
+  }
+}
 
 
 const db = getFirestore(firebase_app);
@@ -47,6 +89,27 @@ const signedUsersSnapshot=await getDocs(postsQuery)
     }
   }, [linkID, router]);
 
+
+  const handleDownloadCSVFile=()=>{
+    const headingOrder=['boostedLinkId', 'createdAt', 'email', 'name', 'providerId']
+    console.log(data.map(Object.values))
+
+  
+   const excelData=data.map((obj: any) =>
+    headingOrder.map((key: any) =>{
+      if(key=="createdAt"){
+        return new Date(obj[key].seconds * 1000 + obj[key].nanoseconds / 1e6).toLocaleDateString()
+      }
+      return  obj[key]
+    })
+  )
+
+    exportToCsv(`${linkID}.csv`,  [
+      headingOrder,	
+      excelData,    
+    ])
+  }
+
   if (loading) {
     return <CircularProgress
     style={{ position: "fixed", left: "50vw", top: "35vh" }}
@@ -54,6 +117,9 @@ const signedUsersSnapshot=await getDocs(postsQuery)
   }
 
   return (
+    <>
+    <Button style={{margin:"0 auto", marginTop:"30px", marginBottom:"30px", marginLeft:"45vw"}} onClick={handleDownloadCSVFile}>Download Data</Button>
+ 
     <TableContainer component={Paper}>
       <Table>
         <TableHead>
@@ -74,6 +140,7 @@ const signedUsersSnapshot=await getDocs(postsQuery)
         </TableBody>
       </Table>
     </TableContainer>
+    </>
   );
 };
 
