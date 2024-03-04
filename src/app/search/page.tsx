7c53d1fc8ca3236a930b23e55cfc187b8c28f9aa
Alred <prottoyrudra7777@gmail.com>
2024-03-04 20:26:02 +0000

    working on router query

---
+++ src/app/search/page.tsx
@@ -0,0 +1,85 @@
+"use client"; // This is a client component 👈🏽
+
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
+import { TextField } from "@mui/material";
+import { articleSearchQuery } from "../../firebase/firestore/queries";
+import { Box, Typography, Stack } from "@mui/material";
+import { ArticlesData } from "../../types/firestore";
+import { v4 as uuidv4 } from "uuid";
+import CircularProgress from "@mui/material/CircularProgress";
+import { articlesBannerImage } from "../../constants/img-src";
+
+export default function SearchPage() {
+  const router = useRouter();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [articles, setArticles] = useState<ArticlesData[]>([]);
+
+  useEffect(() => {
+    if (searchQuery) {
+      const fetchData = async () => {
+        await getDocs(articleSearchQuery(searchQuery))
+          .then((querySnapshot) => {
+            const articlesData: ArticlesData[] = [];
+            querySnapshot.forEach((doc: any) => {
+              articlesData.push(doc.data());
+            });
+            setArticles(articlesData);
+            setLoading(false);
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      };
+
+      fetchData();
+    }
+  }, [searchQuery]);
+
+  useEffect(() => {
+    const { q } = router.query;
+    if (q) {
+      setSearchQuery(q.toString());
+    }
+  }, [router.query]);
+
+  return (
+    <Box p={5}>
+      {loading && (
+        <CircularProgress
+          style={{ position: "fixed", left: "50vw", top: "35vh" }}
+        />
+      )}
+      {!loading && (
+        <Box>
+          <img
+            style={{ margin: "0 auto" }}
+            src={articlesBannerImage}
+            alt="image-1"
+            width={600}
+            height={300}
+          />
+
+          <Box>
+            <Stack spacing={12} padding="20px" mt={10}>
+              {articles.map((article) => (
+                <div key={uuidv4()}>
+                  <Typography variant="h5">{article.title}</Typography>
+                </div>
+              ))}
+            </Stack>
+          </Box>
+        </Box>
+      )}
+    </Box>
+  );
+}
--- src/firebase/firestore/queries.ts
+++ src/firebase/firestore/queries.ts
@@ -1,21 +1,45 @@
-import { collection, query, orderBy, limit, getDocs, doc,where } from "firebase/firestore";
+import {
+  collection,
+  query,
+  orderBy,
+  limit,
+  getDocs,
+  doc,
+  where,
+} from "firebase/firestore";
 import firebase_app from "../config";
 import { getFirestore } from "firebase/firestore";
 
 // Get the Firestore instance
 const db = getFirestore(firebase_app);
 
-
-const articleQuery = query(collection(db, "articles"), orderBy("order"), limit(3));
+const articleQuery = query(
+  collection(db, "articles"),
+  orderBy("order"),
+  limit(3)
+);
 const openAIKeyDoc = doc(db, "APIKeys", "openai-keys");
-const openAIQueryDoc = doc(db, "queries", "openai-queries")
-const boostedLinksQuery =(linkID: string): any=>{
-    return query(collection(db, "boosted-links"), where('id', '==', linkID), limit(1));
-}
+const openAIQueryDoc = doc(db, "queries", "openai-queries");
+const boostedLinksQuery = (linkID: string): any => {
+  return query(
+    collection(db, "boosted-links"),
+    where("id", "==", linkID),
+    limit(1)
+  );
+};
+
+const articleSearchQuery = (title: string): any => {
+  return query(
+    collection(db, "articles"),
+    where("title", "==", title),
+    limit(5)
+  );
+};
 
 export {
-    articleQuery,
-    openAIKeyDoc,
-    openAIQueryDoc,
-    boostedLinksQuery
-}
+  articleQuery,
+  openAIKeyDoc,
+  openAIQueryDoc,
+  boostedLinksQuery,
+  articleSearchQuery,
+};
